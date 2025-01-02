069b6705f41cffb41e345d8cc8db7dae3b709430
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-02 12:02:40 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 # Instalar programas
-sudo pacman -S --noconfirm git go hyprland hyprpaper qt5-wayland qt6-wayland wayland-utils xdg-desktop-portal-hyprland dkms nvidia-dkms intel-ucode grim slurp waybar zsh kitty vim wofi nemo htop papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome neofetch ncspot pavucontrol firefox chromium libreoffice-fresh orchis-theme nwg-look
+sudo pacman -S --noconfirm git go vim hyprland hyprpaper polkit-gnome qt5-wayland qt6-wayland wayland-utils xdg-desktop-portal-hyprland dkms nvidia-dkms intel-ucode grim slurp waybar zsh kitty vim wofi nemo htop papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome neofetch ncspot pavucontrol firefox chromium libreoffice-fresh orchis-theme nwg-look
 
 # Mover .config
 rm -r ~/.config
 mv -f .config ~/
 
 # Mover arquivos .zshrc,.xinitrc, Scripts ...  para ~/
-mv -f .zshrc .zprofile wallpapers Scripts ~/
+mv -f .zshrc wallpapers scripts ~/
 
 # Dar permissões
-#chmod -R 755 ~/.config/hypr/hyprland.conf
-#chmod -R 755 ~/.config/wofi/config.conf
-#chmod -R 755 ~/Scripts/changewp.sh
+chmod -R 755 ~/scripts/changewp.sh
 
 #trocar o shell para zsh
 chsh -s /bin/zsh
@@ -31,5 +29,6 @@ yay -S --noconfirm papirus-folders
 #icones, temas e fontes
 papirus-folders -C white --theme Papirus
 
-rm -rf ~/ly1
+cd
+rm -rf ~/ly
 rm -rf ~/yay
