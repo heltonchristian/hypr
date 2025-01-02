413b392ac8f714b2f3d9f299c0beeff0b4061176
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-02 08:55:08 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Instalar programas
-sudo pacman -S --noconfirm git go hyprland qt5-wayland qt6-wayland wayland-utils desktop-portal-hyprland dkms nvidia-dkms intel-ucode dunst grim slurp waybar zsh kitty vim wmofi nemo htop papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome neofetch ncspot pavucontrol firefox chromium libreoffice-fresh orchis-theme nwg-look
+sudo pacman -S --noconfirm git go hyprland hyprpaper qt5-wayland qt6-wayland wayland-utils xdg-desktop-portal-hyprland dkms nvidia-dkms intel-ucode grim slurp waybar zsh kitty vim wmofi nemo htop papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome neofetch ncspot pavucontrol firefox chromium libreoffice-fresh orchis-theme nwg-look
 
 # Mover .config
 rm -r ~/.config
@@ -14,9 +14,6 @@ mv -f .zshrc .zprofile wallpapers Scripts ~/
 chmod -R 755 ~/.config/hypr/hyprland.conf
 chmod -R 755 ~/.config/wofi/config.conf
 chmod -R 755 ~/Scripts/changewp.sh
-
-#mouse
-#echo -e 'Section "InputClass"\n     Identifier "My Mouse"\n     MatchIsPointer "yes"\n     Option "AccelerationProfile" "-1"\n     Option "AccelerationScheme" "none"\n     Option "AccelSpeed" "-1"\nEndSection' | sudo tee /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
 
 #trocar o shell para zsh
 chsh -s /bin/zsh
