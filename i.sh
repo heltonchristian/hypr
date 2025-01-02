f6ce89cbb3b0224bb8b23f5cb93190665be72ec5
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-02 09:04:52 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Instalar programas
-sudo pacman -S --noconfirm git go hyprland hyprpaper qt5-wayland qt6-wayland wayland-utils xdg-desktop-portal-hyprland dkms nvidia-dkms intel-ucode grim slurp waybar zsh kitty vim wmofi nemo htop papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome neofetch ncspot pavucontrol firefox chromium libreoffice-fresh orchis-theme nwg-look
+sudo pacman -S --noconfirm git go hyprland hyprpaper qt5-wayland qt6-wayland wayland-utils xdg-desktop-portal-hyprland dkms nvidia-dkms intel-ucode grim slurp waybar zsh kitty vim wofi nemo htop papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome neofetch ncspot pavucontrol firefox chromium libreoffice-fresh orchis-theme nwg-look
 
 # Mover .config
 rm -r ~/.config
