4192d2b42c72b3550bcead9f28b7aa0058c085f3
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-03 09:31:03 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Instalar programas
-sudo pacman -S --noconfirm git go vim hyprland egl-wayland hyprpaper polkit-gnome qt5-wayland qt6-wayland wayland-utils xdg-desktop-portal-hyprland dkms nvidia-dkms libva-nvidia-driver lib32-nvidia-utils intel-ucode grim slurp waybar zsh kitty vim wofi nemo htop papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome neofetch ncspot pavucontrol firefox chromium libreoffice-fresh orchis-theme nwg-look
+sudo pacman -S --noconfirm git go vim hyprland egl-wayland hyprpaper polkit qt5-wayland qt6-wayland wayland-utils xdg-desktop-portal-hyprland dkms nvidia-dkms libva-nvidia-driver lib32-nvidia-utils intel-ucode grim slurp waybar zsh kitty vim wofi nemo htop papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome neofetch ncspot pavucontrol firefox libreoffice-fresh orchis-theme nwg-look
 
 # Mover .config
 rm -r ~/.config
 mv -f .config ~/
 
 # Mover arquivos .zshrc,.xinitrc, Scripts ...  para ~/
-mv -f .zshrc wallpapers scripts ~/
+mv -f .zshrc .zprofile wallpapers scripts ~/
 
 # Dar permissões
 chmod -R 755 ~/scripts/changewp.sh
