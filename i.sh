f5cbe123cb4e82fb726374a6aa0de19abccff163
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-03 12:12:41 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -33,3 +33,8 @@ cd
 rm -rf ~/ly
 rm -rf ~/yay
 cd
+
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+echo "[Service]
+ExecStart=
+ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin ly - \$TERM" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf > /dev/null
