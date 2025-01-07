58d568af0950fda38f75f706261893480946b7dc
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-07 16:21:14 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -40,7 +40,7 @@ rm -rf ~/yay
 cd
 
 # Autologin
-sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
-echo "[Service]
-ExecStart=
-ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin ly - \$TERM" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf > /dev/null
+#sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+#echo "[Service]
+#ExecStart=
+#ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin ly - \$TERM" | sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf > /dev/null
