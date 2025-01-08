78eb67e8d713562a5f4b8d5767e7d864cc21c798
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-08 14:57:17 -0300

    Add files via upload

--- scripts/changewpH.sh
+++ scripts/changewpH.sh
@@ -4,17 +4,13 @@ WALLPAPER_DIRECTORY=~/wallpapers
 
 WALLPAPER=$(find "$WALLPAPER_DIRECTORY" -type f | shuf -n 1)
 
-hyprctl hyprpaper preload "$WALLPAPER"
-hyprctl hyprpaper wallpaper "DP-1,$WALLPAPER"
-hyprctl hyprpaper wallpaper "DVI-I-1,$WALLPAPER"
-
-sleep 1
-
-hyprctl hyprpaper unload unused
-
 HYPRPAPER_CONF="$HOME/.config/hypr/hyprpaper.conf"
 echo " " > $HYPRPAPER_CONF
 echo "preload = $WALLPAPER" >> $HYPRPAPER_CONF
 echo "wallpaper = DP-1, $WALLPAPER" >> $HYPRPAPER_CONF
 echo "wallpaper = DVI-I-1, $WALLPAPER" >> $HYPRPAPER_CONF
 echo "splash = false" >> $HYPRPAPER_CONF
+
+killall hyprpaper
+hyprpaper &
+
