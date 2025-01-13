e1db0d2556185f5f7acf2df12a9f4484e63dc0ee
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-12 21:29:08 -0300

    Add files via upload

--- scripts/changewpH.sh
+++ scripts/changewpH.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 WALLPAPER_DIRECTORY=~/wallpapers
-
-WALLPAPER=$(find "$WALLPAPER_DIRECTORY" -type f | shuf -n 1)
-
+WALLPAPERS=($(ls -1 "$WALLPAPER_DIRECTORY"))
+NUM_WALLPAPERS=${#WALLPAPERS[@]}
 HYPRPAPER_CONF="$HOME/.config/hypr/hyprpaper.conf"
+INDEX_FILE="$HOME/.config/hypr/wallpaper_index.txt"
+
+if [ ! -f "$INDEX_FILE" ]; then
+    echo 0 > "$INDEX_FILE"
+fi
+
+INDEX=$(cat "$INDEX_FILE")
+WALLPAPER="$WALLPAPER_DIRECTORY/${WALLPAPERS[$INDEX]}"
+
 echo " " > $HYPRPAPER_CONF
 echo "preload = $WALLPAPER" >> $HYPRPAPER_CONF
 echo "wallpaper = DP-1, $WALLPAPER" >> $HYPRPAPER_CONF
@@ -13,4 +21,7 @@ echo "splash = false" >> $HYPRPAPER_CONF
 
 killall hyprpaper
 hyprpaper &
+
+NEXT_INDEX=$(( (INDEX + 1) % NUM_WALLPAPERS ))
+echo $NEXT_INDEX > "$INDEX_FILE"
 
