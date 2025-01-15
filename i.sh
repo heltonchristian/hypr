1fda63d45601e33ff5bd2e6da8341aafc9612a1b
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-15 16:12:35 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Instalar programas
-sudo pacman -S --noconfirm git go hyprland hyprpaper xdg-desktop-portal-hyprland waybar rofi kitty zsh vim nwg-look remmina freerdp krita
+sudo pacman -S --noconfirm git go hyprland hyprpaper xdg-desktop-portal-hyprland waybar rofi kitty zsh vim remmina freerdp
 
 sudo pacman -S --noconfirm nvidia-dkms nvidia-utils libva libva-nvidia-driver lib32-nvidia-utils
 
-sudo pacman -S --noconfirm nemo nemo-fileroller grim slurp papirus-icon-theme neofetch firefox orchis-theme ttf-font-awesome ttf-fira-code steam spotify-launcher
+sudo pacman -S --noconfirm grim slurp papirus-icon-theme neofetch firefox orchis-theme ttf-font-awesome ttf-nerd-fonts-symbols steam spotify-launcher
 
 # Mover .config
 rm -r ~/.config
@@ -25,7 +25,7 @@ makepkg -si
 cd
 
 #AUR programas
-yay -S --noconfirm papirus-folders hyprshot
+yay -S --noconfirm papirus-folders hyprshot librewolf-bin
 
 #icones, temas e fontes
 papirus-folders -C black --theme Papirus
