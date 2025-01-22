c1f1275993ef154406282332562d105c6c7f5fd2
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-01-22 20:59:20 +0000

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Instalar programas
-sudo pacman -S  git go xorg-server xorg-apps hyprland hyprpaper xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland waybar kitty zsh vim remmina freerdp nwg-look gammastep nvidia-settings
-sudo pacman -S  grim slurp papirus-icon-theme neofetch orchis-theme ttf-font-awesome ttf-nerd-fonts-symbols steam spotify-launcher
+sudo pacman -S  --noconfirm git go xorg-server xorg-apps hyprland hyprpaper xdg-desktop-portal-hyprland waybar kitty zsh vim remmina freerdp nwg-look
+sudo pacman -S  --noconfirm grim slurp papirus-icon-theme neofetch orchis-theme ttf-font-awesome ttf-nerd-fonts-symbols steam spotify-launcher
 
 # Mover .config
 rm -r ~/.config
@@ -22,7 +22,7 @@ makepkg -si
 cd
 
 #AUR programas
-yay -S --removemake --noconfirm papirus-folders hyprshot librewolf-bin tofi wl-gammarelay
+yay -S --removemake --noconfirm papirus-folders hyprshot librewolf-bin tofi wl-gammarelay-rs
 
 #icones, temas e fontes
 papirus-folders -C black --theme Papirus
