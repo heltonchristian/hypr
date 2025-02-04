7c165965dafeee1728b68fcca9080e2c8b3010fb
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-02-04 12:53:02 +0000

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Instalar programas
-sudo pacman -S  --noconfirm git go hyprland hyprpaper waybar alacritty zsh neovim nwg-look ttf-nerd-fonts-symbols nemo nemo-fileroller
+sudo pacman -S  --noconfirm git go hyprland hyprpaper waybar alacritty zsh neovim nwg-look ttf-nerd-fonts-symbols nemo nemo-fileroller vlc
 sudo pacman -S  --noconfirm grim slurp tela-circle-icon-theme-black neofetch orchis-theme steam ttf-fira-code spotify-launcher
 #sudo pacman -S --noconfirm remmina freerdp
 
