c2d00e33edc03c1c62b550a619a56dc2c67b60f1
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-09-03 10:05:05 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,27 +1,21 @@
 #!/bin/sh
 
-# Instalar programas
 sudo pacman -S --noconfirm git go hyprland hyprpaper waybar alacritty zsh neovim ttf-nerd-fonts-symbols nemo nemo-fileroller vlc papirus-icon-theme ttf-fira-code orchis-theme
 sudo pacman -S --noconfirm qemu virt-manager libvirt edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat virt-viewer spice-server spice-gtk freerdp
 
-# Mover .config
 rm -r ~/.config
 mv -f .config ~/
 
-# Mover arquivos .zshrc, .zprofile e scripts para ~/
 mv -f .zshrc .zprofile scripts ~/
 
-# Dar permissões
 chmod +x ~/scripts/changewpH.sh
 chmod +x ~/scripts/changeAudio.sh
 
-# AUR Helper
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd
 
-# AUR programas
 yay -S --noconfirm --quiet --needed hyprshot tofi wl-gammarelay-rs cava bibata-cursor-theme librewolf-bin
 
 cd
@@ -29,5 +23,4 @@ rm -rf ~/hypr
 rm -rf ~/yay
 cd
 
-# Trocar o shell para zsh
 chsh -s /bin/zsh
