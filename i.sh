15988521486b2a6f9a26a77dc3298ee0eec0b43d
-ly <91619541+heltonchristian@users.noreply.github.com>
2025-09-03 01:00:43 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,64 +1,32 @@
 #!/bin/sh
-set -e
 
-echo "==> Atualizando sistema"
-sudo pacman -Syu --noconfirm
+# Instalar programas
+sudo pacman -S --noconfirm git go hyprland hyprpaper waybar alacritty zsh neovim ttf-nerd-fonts-symbols nemo nemo-fileroller vlc
 
-echo "==> Instalando pacotes básicos do sistema e Hyprland"
-sudo pacman -S --noconfirm --needed \
-  hyprland hyprpaper waybar alacritty grim slurp firefox\
-  noto-fonts-extra ttf-nerd-fonts-symbols ttf-fira-code \
-  papirus-icon-theme orchis-theme libnotify \
-  dolphin vlc fastfetch btop \
-  curl dialog freerdp iproute2 gnu-netcat \
-  git go neovim zsh
+# Mover .config
+rm -r ~/.config
+mv -f .config ~/
 
-echo "==> Backup e cópia de configs"
-mv ~/.config ~/.config.bak.$(date +%s) || true
-cp -r .config ~/
-cp -f .zshrc .zprofile -t ~/
-cp -r scripts wallpapers -t ~/
-chmod +x ~/scripts/*.sh
+# Mover arquivos .zshrc, .zprofile e scripts para ~/
+mv -f .zshrc .zprofile scripts ~/
 
-echo "==> Instalando yay (AUR helper)"
+# Dar permissões
+chmod +x ~/scripts/changewpH.sh
+chmod +x ~/scripts/changeAudio.sh
+
+# AUR Helper
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si --noconfirm
-cd ..
-rm -rf yay
+makepkg -si
+cd
 
-echo "==> Instalando pacotes AUR estáveis"
-yay -S --noconfirm --needed wl-gammarelay-rs cava bibata-cursor-theme tofi
+# AUR programas
+yay -S --noconfirm --quiet --needed hyprshot tofi wl-gammarelay-rs cava bibata-cursor-theme 
 
-echo "==> Configurando autologin no tty1"
-sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
-sudo cp -f override.conf /etc/systemd/system/getty@tty1.service.d/
+cd
+rm -rf ~/hypr
+rm -rf ~/yay
+cd
 
-echo "==> Trocando shell padrão para zsh"
+# Trocar o shell para zsh
 chsh -s /bin/zsh
-
-echo "==> Instalando pacotes de jogos"
-sudo pacman -S --noconfirm --needed \
-  steam goverlay \
-  mangohud gamemode \
-  mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon \
-  vulkan-icd-loader lib32-vulkan-icd-loader \
-  wine winetricks obs-studio \
-  pipewire pipewire-pulse pipewire-alsa wireplumber
-
-echo "==> Instalando pacotes de virtualização"
-sudo pacman -S --noconfirm --needed \
-  qemu-full virt-manager edk2-ovmf virt-viewer \
-  dnsmasq vde2 bridge-utils openbsd-netcat iptables-nft
-
-echo "==> Ativando libvirtd"
-sudo systemctl enable --now libvirtd
-sudo usermod -aG libvirt $(whoami)
-
-echo "==> Aplicando tema e ícones (Orchis + Papirus + Bibata)"
-gsettings set org.gnome.desktop.interface gtk-theme "Orchis-Dark"
-gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
-gsettings set org.gnome.desktop.interface cursor-theme "Bibata-Modern-Ice"
-
-echo "==> Finalizado!"
-echo "Reinicie o sistema para aplicar todas as mudanças."
